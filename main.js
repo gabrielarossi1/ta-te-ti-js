27827eef89dbd9ee575f01e070faf68aade2969d
Gabriela Rossi <gabi_20_14@hotmail.com>
2023-06-23 00:07:33 -0300

    css listo

--- main.js
+++ main.js
@@ -3,3 +3,20 @@ const board = [
     ['', '', ''],
     ['', '', '']
 ]; //arreglo del tablero, 3 filas, 3 columnas
+
+let turn = 0; //0 = user, 1 =PC
+const boardContainer = document.querySelector('#board');
+const playerDiv = document.querySelector('#player');
+
+renderBoard();
+
+function renderBoard(){
+    const html = board.map(row =>{
+        const cells = row.map(cell =>{
+            return `<button class="cell">${cell}</button>`; //dibujamos los botoncitos
+        });
+        return `<div class="row">${cells.join('')}</div>`;
+    });
+
+    boardContainer.innerHTML = html.join("");
+}
